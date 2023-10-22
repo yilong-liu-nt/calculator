72427f0b11b153102307cbf81eb923bc0beb9778
yilong-liu-nt <147955346+yilong-liu-nt@users.noreply.github.com>
2023-10-22 18:35:21 -0400

    image
    W.I.P, added image adding, but nothing else can be done, install PIL

--- image.py
+++ image.py
@@ -1,2 +1,38 @@
+from tkinter import *
+from tkinter import filedialog
+from functools import partial
+
+from PIL import Image, ImageTk
+
+img = None
+
+def upload_image(gui):
+    global img
+
+    file_types = [('Png Files', '*.Png'), ('Jpg Files', '*.Jpg')]
+    filename = filedialog.askopenfilename(filetypes=file_types)
+    img = ImageTk.PhotoImage(file=filename)
+    print(filename)
+    if img is not None:
+        print("Here is image")
+    # b1 = Button(gui, text="Upload Image", image=img)
+
+    Label(gui, image=img).grid(row=1, column=0)
+
+
 def imageWindow(master):
-    pass
+    global img
+    gui = Toplevel(master)
+
+    # set the background colour of GUI window
+    gui.configure(background="light green")
+
+    # set the title of GUI window
+    gui.title("Image Calculator")
+
+    # set the configuration of GUI window
+    gui.geometry("540x300")
+
+    upload_button = Button(gui, text='Upload Image',
+                           command=partial(upload_image, gui)).grid(row=0, column=0)
+    
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     master.title("Calculator Home Screen")
     master.geometry("500x600")
 
-    label = Label(master, text= "Welcome")
+    label = Label(master, text= "Welcome to use the following calculators.").grid(row=0, column=0, columnspan=2)
 
 
     # a button widget which will open a
@@ -21,17 +21,19 @@ if __name__ == "__main__":
             text="Basic",
             height= 5, width = 20,
             command=partial(basicWindow, master)
-            ).grid(row=0, column=0,)
+            ).grid(row=1, column=0)
+    
     btn2 = Button(master,
             text="Image",
             height= 5, width = 20,
             command=partial(imageWindow, master)
-            ).grid(row=3, column=0)
+            ).grid(row=2, column=0)
+    
     btn3 = Button(master,
             text="Text",
             height= 5, width = 20,
             command=partial(textWindow, master)
-            ).grid(row=5, column=0,)
+            ).grid(row=3, column=0,)
 
     # start the GUI
     master.mainloop()
