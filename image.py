30b9ce4e2997879fbd1e8e42fedd7c0fffaf6e3c
yilong-liu-nt <147955346+yilong-liu-nt@users.noreply.github.com>
2023-10-29 18:45:33 -0400

    finished image calctotor
    you set the RGB yeeeeee

--- image.py
+++ image.py
@@ -101,12 +101,74 @@ def edge_image(gui):
     resultImage = ImageTk.PhotoImage(edge_restored_image)
 
     Label(gui, image=resultImage).grid(row=3, column=2)
+    
+def set_r(gui):
+    global resultImage
+    global target_R
+
+    try:
+        target_r_value = int(target_R.get())
+    except:
+        target_r_value = 0
+    
+    my_image = ImageTk.getimage(resultImage)
+    array_1 = np.asarray(my_image).copy()  # (256, 256, 4)
+    # 0: R
+    # 1: G
+    # 2: B
+    # 3: transparent
+    array_1[:,:,0] = np.ones((256, 256), dtype=int)*target_r_value
+    resultImage = ImageTk.PhotoImage(Image.fromarray(array_1))
+    Label(gui, image=resultImage).grid(row=3, column=2)
+
+
+
+def set_g(gui):
+    global resultImage
+    global target_G
+
+    try:
+        target_g_value = int(target_G.get())
+    except:
+        target_g_value = 0
+    
+    my_image = ImageTk.getimage(resultImage)
+    array_1 = np.asarray(my_image).copy()  # (256, 256, 4)
+    # 0: R
+    # 1: G
+    # 2: B
+    # 3: transparent
+    array_1[:,:,1] = np.ones((256, 256), dtype=int) * target_g_value
+    resultImage = ImageTk.PhotoImage(Image.fromarray(array_1))
+    Label(gui, image=resultImage).grid(row=3, column=2)
+
+def set_b(gui):
+    global resultImage
+    global target_B
+
+    try:
+        target_b_value = int(target_B.get())
+    except:
+        target_b_value = 0
+    
+    my_image = ImageTk.getimage(resultImage)
+    array_1 = np.asarray(my_image).copy()  # (256, 256, 4)
+    # 0: R
+    # 1: G
+    # 2: B
+    # 3: transparent
+    array_1[:,:,2] = np.ones((256, 256), dtype=int)*target_b_value
+    resultImage = ImageTk.PhotoImage(Image.fromarray(array_1))
+    Label(gui, image=resultImage).grid(row=3, column=2)
 
 def imageWindow(master):
     global img1
     global img2
     global resultImage
-    
+    global target_R
+    global target_G
+    global target_B
+
     gui = Toplevel(master)
 
     # set the background colour of GUI window
@@ -123,9 +185,12 @@ def imageWindow(master):
     button_minus = Button(gui, text='+', command=partial(image_add, gui)).grid(row=0, column=1)
     button_minus = Button(gui, text='Transpose', command=partial(image_transpose, gui)).grid(row=4, column=0)
 
-    remove_color_r = Button(gui, text='Remove R').grid(row=1, column=0)
-    remove_color_g = Button(gui, text='Remove G').grid(row=1, column=1)
-    remove_color_b = Button(gui, text='Remove B').grid(row=1, column=2)
+    set_color_r = Button(gui, text='Set R',
+                            command=partial(set_r, gui)).grid(row=1, column=0)
+    set_color_g = Button(gui, text='Set G',
+                            command=partial(set_g, gui)).grid(row=1, column=1)
+    set_color_b = Button(gui, text='Set B',
+                            command=partial(set_b, gui)).grid(row=1, column=2)
     
     
     upload_button_1 = Button(gui, text='Upload Image 1',
@@ -139,9 +204,20 @@ def imageWindow(master):
     edge_button= Button(gui, text='Edge Detection',
                            command=partial(edge_image, gui)).grid(row=4, column=2)
     
-    
     Label(gui, text = "Result").grid(row=2, column=2)
 
+    target_R = StringVar()
+    target_G = StringVar()
+    target_B = StringVar()
+    target_R.set("R")
+    target_G.set("G")
+    target_B.set("B")
+    entry_R = Entry(gui, textvariable=target_R).grid(row=6, column=0)
+    entry_G = Entry(gui, textvariable=target_G).grid(row=6, column=1)
+    entry_B = Entry(gui, textvariable=target_B).grid(row=6, column=2)
+    Label_R = Label(gui, text='Input R: 0-255').grid(row=7, column=0)
+    Label_G = Label(gui, text='Input G: 0-255').grid(row=7, column=1)
+    Lable_B = Label(gui, text='Input B: 0-255').grid(row=7, column=2)
     img=Image.new("RGB", image_size, (255, 255, 255))
     img1=ImageTk.PhotoImage(img)
     Label(gui, image=img1).grid(row=3, column=0)
